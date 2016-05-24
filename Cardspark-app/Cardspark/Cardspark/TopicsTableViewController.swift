a3a923c4b1792e05e1eb7456ba5d83e8ace821d5
Leanne <leanne_lyons@hotmail.co.uk>
2016-05-24 18:02:18 +0100

    Added a messenger tab

--- Cardspark-app/Cardspark/Cardspark/TopicsTableViewController.swift
+++ Cardspark-app/Cardspark/Cardspark/TopicsTableViewController.swift
@@ -14,13 +14,15 @@ class TopicsTableViewController: UITableViewController {
     var topics = [Topic]()
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+      super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+      
+      navigationItem.leftBarButtonItem = editButtonItem()
       
       loadSampleData()
     }
@@ -83,25 +85,27 @@ class TopicsTableViewController: UITableViewController {
     }
  
 
-    /*
+  
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+  
 
-    /*
+  
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+          
+            topics.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
