b9b6830519ae196a125cfcf73e72febc746a1fcf
Glen Rodgers <glenmrodgers@gmail.com>
2016-05-25 16:07:50 +0100

    [GR] Updated controllers to handle requests

--- Cardspark-app/cs_backend/app/controllers/topics_controller.rb
+++ Cardspark-app/cs_backend/app/controllers/topics_controller.rb
@@ -1,2 +1,20 @@
 class TopicsController < ApplicationController
+    def index
+	    @topics = Topic.includes(:users).where('users.id' => params[:user_id])
+	    render :nothing => @topics
+    end
+
+    def create
+    	@topic = Topic.new(topic_params)
+    	if @topic.save
+		    redirect_to @topic
+		  else
+		    render 'new'
+		  end
+    end
+
+	private
+	  def topic_params
+	    params.require(:topic).permit(:name)
+	  end
 end
