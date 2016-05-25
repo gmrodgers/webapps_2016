689a3956b6551455ede3f47d4a4f58901ba16b63
Glen Rodgers <glenmrodgers@gmail.com>
2016-05-25 16:08:45 +0100

    [GR] Added unit tests for two of the models and the topics controller

--- Cardspark-app/cs_backend/test/models/topic_test.rb
+++ Cardspark-app/cs_backend/test/models/topic_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class TopicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save topic without name" do
+	  topic = Topic.new
+	  assert_not topic.save
+  end
+
+  test "should save topic with name" do
+	  topic = Topic.new(name: "os")
+	  assert topic.save
+  end
+
 end
--- Cardspark-app/cs_backend/test/models/user_test.rb
+++ Cardspark-app/cs_backend/test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "should not create user without email" do
+		user = User.new(password_hash: "password")
+		assert_not user.save
+	end
+
+	test "should not create user without password_hash" do
+		user = User.new(email: "john@example.com")
+		assert_not user.save
+	end
+
 end
