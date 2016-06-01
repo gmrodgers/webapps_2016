ab265938678cb5ed316d85f224a27ded27fd8f96
willaboh <willaboh@gmail.com>
2016-06-01 02:48:17 +0000

    Edits create to return status codes and errors

--- Cardspark-app/backend/app/controllers/users_controller.rb
+++ Cardspark-app/backend/app/controllers/users_controller.rb
@@ -1,58 +1,38 @@
 class UsersController < ApplicationController
 	skip_before_filter  :verify_authenticity_token
+	# should check that the above does not lead to security issues
 	respond_to :json
-
-	def index
-		@users = User.all
-  	render :json => @users
-	end
-
-	def create
+	
+  def create
     @user = User.new(user_params)
-    respond_to do |format|
-      if @user.save
-      	redirect_to @user
-      else
-        render 'new'
-      end
-    end
-  end
-
-  def new
-  	@user = User.new
-  	render :json => @user	
-  end
-
-  def edit
-    @user = User.find(params[:id])
-    render :json => @user
-  end
-
-  def show
-    @user = User.find(params[:id])
-  	render :json => @user
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to @user
+    if @user.save
+      render :json => @user, status: :created
     else
-      render 'edit'
+      render :json => { :errors => @user.errors.full_messages },
+                      status: :unprocessable_entity
     end
-    render :json => @user
   end
 
-  def destroy
-	  @user = User.find(params[:id])
-	  @user.destroy
-	  redirect_to users_path
-	end 
+#   def update
+#     @user = User.find(params[:id])
+#     if @user.update(user_params)
+#       redirect_to @user
+#     else
+#       render 'edit'
+#     end
+#     render :json => @user
+#   end
 
-  def utopics
-    @topics = User.find(params[:id]).topics
-    render :json => @topics 
-  end
+#   def destroy
+# 	  @user = User.find(params[:id])
+# 	  @user.destroy
+# 	  redirect_to users_path
+# 	end 
+
+#   def utopics
+#     @topics = User.find(params[:id]).topics
+#     render :json => @topics 
+#   end
 
 private 
   def user_params
