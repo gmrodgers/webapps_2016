5b3b5bd451364bce51436eb073d437e66dc80ed9
willaboh <willaboh@gmail.com>
2016-06-01 08:15:12 +0000

    Tests index action for topics

--- Cardspark-app/backend/test/controllers/topics_controller_test.rb
+++ Cardspark-app/backend/test/controllers/topics_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class TopicsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should list all topics belonging to particular user" do
+    #user and topics defined in fixture
+    self.use_instantiated_fixtures = true
+    get :index, user_email: users(:user).email
+    assert_response :success
+    assert_not_nil assigns(:topics)
+  end
+  
 end
