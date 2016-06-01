ffcfe3d43577efbceab88c24a259038d6f74ff83
willaboh <willaboh@gmail.com>
2016-06-01 02:50:54 +0000

    Adds tests for user email presence and uniqueness

--- Cardspark-app/backend/test/models/user_test.rb
+++ Cardspark-app/backend/test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should not save user without an email" do
+    user = User.new
+    
+    assert_not user.save, "User should not be saved without an email"
+  end
+  
+  test "should not allow duplicate users" do
+    duplicateUser = User.new(email: "test@email.com")
+    
+    # User with same email is present in fixture
+    assert_not duplicateUser.save, "User should not be saved if duplicate exists"
+  end
+  
 end
