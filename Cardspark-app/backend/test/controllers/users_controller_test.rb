0ba85fe56846d567d3ecd162d8dd6ab8a345099a
willaboh <willaboh@gmail.com>
2016-06-01 04:37:31 +0000

    Tests update user action

--- Cardspark-app/backend/test/controllers/users_controller_test.rb
+++ Cardspark-app/backend/test/controllers/users_controller_test.rb
@@ -1,10 +1,22 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  # should add tests for when params are invalid
+   
+  def setup
+    @user = User.new(email: "example@email.com")
+  end
   
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { email: "new_user@email.com" }
+      post :create, user: { email: @user.email }
+    end
+  end
+  
+  test "should update user email" do
+    if @user.save
+      put :update, user: { email: "new_user@email.com" }, email: @user.email 
+      assert_equal "new_user@email.com", User.find(@user.id).email
     end
   end
   
