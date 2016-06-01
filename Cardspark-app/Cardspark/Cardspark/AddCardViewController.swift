bbbbb1375c131e66112e0054122c7fd41e9cdb96
Leanne <leanne_lyons@hotmail.co.uk>
2016-06-01 10:44:48 +0100

    [LL] added character limit to text views, also tried to allow deletion in card table but when you switch views, card reappears

--- Cardspark-app/Cardspark/Cardspark/AddCardViewController.swift
+++ Cardspark-app/Cardspark/Cardspark/AddCardViewController.swift
@@ -8,78 +8,72 @@
 
 import UIKit
 
-class AddCardViewController: UIViewController {
-
-    @IBOutlet weak var titleTextField: UITextView!
-    
-    @IBOutlet weak var point1TextField: UITextView!
-    
-    @IBOutlet weak var point2TextField: UITextView!
-    
-    @IBOutlet weak var point3TextField: UITextView!
-    
-    @IBAction func saveCard(sender: AnyObject) {
-        
-        var html: String = "<h3><center>\(titleTextField.text)</h3></center>"
-        html += "<p>\(point1TextField.text)</p>"
-        html += "<p>\(point2TextField.text)</p>"
-        html += "<p>\(point3TextField.text)</p>"
-        
-        let fmt = UIMarkupTextPrintFormatter(markupText: html)
-        let render = UIPrintPageRenderer()
-        render.addPrintFormatter(fmt, startingAtPageAtIndex: 0)
-        let page = CGRect(x: 0, y: 0, width: 595.2, height: 841.8)
-        let printable = CGRectInset(page, 0, 0)
-        
-        render.setValue(NSValue(CGRect: page), forKey: "paperRect")
-        render.setValue(NSValue(CGRect: printable), forKey: "printableRect")
-        
-        let pdfData = NSMutableData()
-        UIGraphicsBeginPDFContextToData(pdfData, CGRectZero, nil)
-        
-        for i in 1...render.numberOfPages() {
-            
-            UIGraphicsBeginPDFPage();
-            let bounds = UIGraphicsGetPDFContextBounds()
-            render.drawPageAtIndex(i - 1, inRect: bounds)
-        }
-        
-        UIGraphicsEndPDFContext();
-        
-        let documentsPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
-        
-        pdfData.writeToFile("\(documentsPath)/\(titleTextField.text).pdf", atomically: true)
-        
-        print("saved")
-        
-        self.dismissViewControllerAnimated(true, completion: nil)
-        
-    }
-    
-    @IBAction func dissmissAddCard(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
+class AddCardViewController: UIViewController, UITextViewDelegate {
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  @IBOutlet weak var titleTextField: UITextView!
+  @IBOutlet weak var point1TextField: UITextView!
+  @IBOutlet weak var point2TextField: UITextView!
+  @IBOutlet weak var point3TextField: UITextView!
+  
+  let COMMENTS_LIMIT = 140
+  
+  // MARK: UITextViewDelegate
+  func textView(textView: UITextView, shouldChangeTextInRange range:NSRange, replacementText text:String ) -> Bool {
+    return textView.text.characters.count + (text.characters.count - range.length) <= COMMENTS_LIMIT;
+    
+  }
+  
+  @IBAction func saveCard(sender: AnyObject) {
+    
+    var html: String = "<h3><center>\(titleTextField.text)</h3></center>"
+    html += "<p>\(point1TextField.text)</p>"
+    html += "<p>\(point2TextField.text)</p>"
+    html += "<p>\(point3TextField.text)</p>"
+    
+    let fmt = UIMarkupTextPrintFormatter(markupText: html)
+    let render = UIPrintPageRenderer()
+    render.addPrintFormatter(fmt, startingAtPageAtIndex: 0)
+    let page = CGRect(x: 0, y: 0, width: 595.2, height: 841.8)
+    let printable = CGRectInset(page, 0, 0)
+    
+    render.setValue(NSValue(CGRect: page), forKey: "paperRect")
+    render.setValue(NSValue(CGRect: printable), forKey: "printableRect")
+    
+    let pdfData = NSMutableData()
+    UIGraphicsBeginPDFContextToData(pdfData, CGRectZero, nil)
+    
+    for i in 1...render.numberOfPages() {
+      
+      UIGraphicsBeginPDFPage();
+      let bounds = UIGraphicsGetPDFContextBounds()
+      render.drawPageAtIndex(i - 1, inRect: bounds)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    UIGraphicsEndPDFContext();
+    
+    let documentsPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+    
+    pdfData.writeToFile("\(documentsPath)/\(titleTextField.text).pdf", atomically: true)
+    
+    print("saved")
+    
+    self.dismissViewControllerAnimated(true, completion: nil)
+    
+  }
+  
+  @IBAction func dissmissAddCard(sender: AnyObject) {
+    self.dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    point1TextField.delegate = self
+    point2TextField.delegate = self
+    point3TextField.delegate = self
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 }
