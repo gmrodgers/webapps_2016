3852ecc05bbb73199234badd53e107b2aced1a39
willaboh <willaboh@gmail.com>
2016-06-02 23:09:19 +0000

    Refactors and strengthens tests

--- Cardspark-app/backend/test/controllers/users_controller_test.rb
+++ Cardspark-app/backend/test/controllers/users_controller_test.rb
@@ -2,28 +2,27 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   # should add tests for when params are invalid
-   
-  def setup
-    @user = User.new(email: "example@email.com")
-  end
   
   test "should create user" do
     assert_difference('User.count') do
+      @user = User.new(email: "example@email.com")
       post :create, user: { email: @user.email }
     end
   end
   
   test "should update user email" do
-    if @user.save
-      put :update, user: { email: "new_user@email.com" }, email: @user.email 
-      assert_equal "new_user@email.com", User.find(@user.id).email
-    end
+    user = users(:user)
+    put :update, user: { email: "new_user@email.com" }, email: user.email 
+    assert_equal "new_user@email.com", User.find(user.id).email
   end
   
-  test "should delete user" do
-    assert_no_difference('User.count') do
-      post :create, user: { email: @user.email }
-      delete :destroy, email: @user.email
+  # Need to test that if user owns a personal topic, the topic is also deleted
+  test "should delete user and remove associations with topics" do
+    assert_difference('User.count', -1) do
+      topic = topics(:topic1)
+      assert_difference('topic.users.count', -1) do
+        delete :destroy, email: users(:viewer_user).email
+      end
     end
   end
   
