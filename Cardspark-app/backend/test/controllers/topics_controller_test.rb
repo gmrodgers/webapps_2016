a5cf164f476d6faaa1d08dd04c247a42d1233d54
willaboh <willaboh@gmail.com>
2016-06-02 18:21:49 +0000

    Tests returning of id after topic creation

--- Cardspark-app/backend/test/controllers/topics_controller_test.rb
+++ Cardspark-app/backend/test/controllers/topics_controller_test.rb
@@ -14,21 +14,30 @@ class TopicsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:topics)
   end
   
-  test "should create new topic and relation to user" do
+  test "should create new topic, relation to user and return topic_id" do
     @topic = Topic.new(name: "new_topic")
     assert_difference('Topic.count') do
       assert_difference('users(:user).topics.count') do
         post :create, topic: { name: @topic.name }, user_email: users(:user).email
       end
     end
+    value = JSON.parse(@response.body)
+    assert_not_nil value['id']
+    assert_equal "created", value['status']
   end
   
-  test "should add user relation to topic in association records" do
+  test "should add relation between user and topic in association records" do
     assert_difference('topics(:topic2).users.count') do
       assert_difference('users(:viewer_user).topics.count') do
         post :add_viewer, email: users(:viewer_user).email, id: topics(:topic2).id
       end
     end
   end
+  
+  # test "should update topic name" do
+  #   id = 
+  #   put :update, user_email: users(:user).email, 
+  #   assert_equal "new_title", topics(:topic1).name, 
+  # end
   
 end
