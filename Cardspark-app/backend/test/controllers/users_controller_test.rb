c8c6530cf34f892623ccb834060c23ac62a8eb9e
willaboh <willaboh@gmail.com>
2016-06-03 00:14:18 +0000

    Adds test for deletion of topic when sole user is deleted

--- Cardspark-app/backend/test/controllers/users_controller_test.rb
+++ Cardspark-app/backend/test/controllers/users_controller_test.rb
@@ -16,13 +16,18 @@ class UsersControllerTest < ActionController::TestCase
     assert_equal "new_user@email.com", User.find(user.id).email
   end
   
-  # Need to test that if user owns a personal topic, the topic is also deleted
   test "should delete user and remove associations with topics" do
     assert_difference('User.count', -1) do
       topic = topics(:topic1)
       assert_difference('topic.users.count', -1) do
         delete :destroy, email: users(:viewer_user).email
       end
+    end
+  end
+  
+  test "should delete topic if user to be deleted is the only owner" do
+    assert_difference('Topic.count', -2) do
+        delete :destroy, email: users(:user).email
     end
   end
   
