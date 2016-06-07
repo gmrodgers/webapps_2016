b05ac5005ee06420ed93d840940606a1d7832a12
willaboh <willaboh@gmail.com>
2016-06-07 03:03:18 +0000

    Adds new helpers

--- Cardspark-app/backend/app/helpers/application_helper.rb
+++ Cardspark-app/backend/app/helpers/application_helper.rb
@@ -4,7 +4,19 @@ module ApplicationHelper
         render :json => { :errors => arg.errors.full_messages }
     end
     
-    def render_instance(arg)
-        render :json => arg, status: :no_content
+    def render_object(arg)
+        render :json => { object: arg }
+    end
+    
+    def render_no_content
+        render :json => { status: :no_content }
+    end
+    
+    def render_not_present
+        render :json => { report: "Record not present in database!" }
+    end
+    
+    def render_id(arg)
+        render :json => { id: arg.id }
     end
 end
