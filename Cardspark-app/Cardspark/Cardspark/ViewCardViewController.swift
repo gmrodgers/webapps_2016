412ea665438fd55bd74ee67ab70e38f70a659f76
willaboh <willaboh@gmail.com>
2016-06-09 17:37:10 +0100

    Implements viewing of cards

--- Cardspark-app/Cardspark/Cardspark/ViewCardViewController.swift
+++ Cardspark-app/Cardspark/Cardspark/ViewCardViewController.swift
@@ -9,59 +9,64 @@
 import UIKit
 
 class ViewCardViewController: UIViewController {
-    
-    var url = NSURL()
-    var cards = [Card]()
-    var index = Int()
-    
-    @IBAction func prevCard(sender: AnyObject) {
-        print("drag right")
-        if index < cards.count-1{
-        index = index+1
-        let urlReq = NSURLRequest(URL: cards[index].url)
-        
-        webview.loadRequest(urlReq)
-        }
-        else{
-            print("index too high")
-        }
-        
+  
+  //    var url = NSURL()
+  var cards = [Card]()
+  var index = Int()
+  
+  @IBAction func prevCard(sender: AnyObject) {
+    print("drag right")
+    if index < cards.count-1{
+      index = index+1
+      //        let urlReq = NSURLRequest(URL: cards[index].url)
+      //
+      //        webview.loadRequest(urlReq)
+      webview.loadHTMLString(cards[index].htmlData, baseURL: nil)
+    }
+    else{
+      print("index too high")
     }
     
+  }
+  
+  
+  @IBAction func nextCard(sender: AnyObject) {
+    print("drag left")
+    if index > 0{
+      index = index-1
+      //            let urlReq = NSURLRequest(URL: cards[index].url)
+      //
+      //            webview.loadRequest(urlReq)
+      webview.loadHTMLString(cards[index].htmlData, baseURL: nil)
+    }
+  }
+  
+  @IBOutlet weak var webview: UIWebView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    //        print(url)
+    //        let urlReq = NSURLRequest(URL: url)
+    //        webview.loadRequest(urlReq)
     
-    @IBAction func nextCard(sender: AnyObject) {
-        print("drag left")
-        if index > 0{
-            index = index-1
-            let urlReq = NSURLRequest(URL: cards[index].url)
-            
-            webview.loadRequest(urlReq)
-        }
-    }
-
-    @IBOutlet weak var webview: UIWebView!
+    webview.loadHTMLString(cards[index].htmlData, baseURL: nil)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print(url)
-        let urlReq = NSURLRequest(URL: url)
-        webview.loadRequest(urlReq)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
 }
