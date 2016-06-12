3772ac39158682dfeca85299dfc3241bb9336f99
Leanne <leanne_lyons@hotmail.co.uk>
2016-06-12 14:27:38 +0100

    Added a view to show current viewers of a topic

---
+++ Cardspark-app/Cardspark/Cardspark/UsersTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  UsersTableViewCell.swift
+//  Cardspark
+//
+//  Created by Leanne Lyons on 12/06/2016.
+//  Copyright © 2016 Mango Productions. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewCell: UITableViewCell {
+  
+  @IBOutlet weak var userLabel: UILabel!
+  
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
